872e0664971fa21b13252c3d464138b7f8e199e9
saylock <cntqpb@mail.ru>
2024-09-16 23:52:41 +0300

    completed practice 2: task 1-2, task 3, task 4, added comments

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
-BMI = weight/height**2 
+BMI = weight/height**2
 """
-weight = input()
-height = input()
 
+weight = input("your weight in kg: ")
+height = input("your height in m: ")
 
-#code here
+# code here
 
+BMI = float(weight) / float(height) ** 2  # adding "float" to w & h to make them float numbers because the input always returns the string
+
+print(f"your Body Mass Index is {round(BMI, 2)}")  # using f-string to format message (same thing as (str(variable) + "message"))
+
+if BMI <= 20:
+    print("you have to eat more fastfood")  # just adding some contex to faceless numbers (aka BMI)
+
+elif 20 < BMI <= 25:
+    print("way to go, man!")
+
+else:
+    print("you should not wear tight jeans")
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,11 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+number = input("please input a 4-digit number: ") # asking a user for a 4-digits number, number is a string
+
+sum_ = 0  # creating variable sum_ ["_" is needed because just "sum" is already existing in python as a function]
+
+for digit in number:  # creating variable "digit" for putting in it all digits in order they are appearing in number
+    sum_ += int(digit)  # adding each digit to "sum_"
+
+print(f"the sum of the digits in your number is: {sum_}")
